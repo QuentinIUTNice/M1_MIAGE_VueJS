cf6c5049aeb5aaa8c25f53a3dcc31418fc5d1174
Quentin <quentin.bertramo@gmail.com>
2021-10-06 08:08:28 +0200

    client

---
+++ client/src/components/Accueil.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Bienvenue</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Accueil",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+small {
+  display: block;
+}
+</style>
