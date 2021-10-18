adbc1bb85c920dfc47c6fca6f24f70d345599a17
Alexandre De_Santa_Barbara <alexdsb34@gmail.com>
2021-10-18 17:04:57 +0200

    Update UI Menu

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <router-link to="/"><button class="menu">ACCUEIL</button></router-link>
-    <router-link to="/restaurants"><button class="menu">TROUVER UN RESTAURANT</button></router-link>
+    <router-link to="/"><button class="menu" id="left_button">ACCUEIL</button></router-link>
+    <router-link to="/restaurants"><button class="menu" id="right_button">TROUVER UN RESTAURANT</button></router-link>
 
     <br /><br />
 
@@ -24,18 +24,28 @@ export default {
   font-size: 100%;
   text-align: center;
   color: #22364b;
-  margin-top: 20px;
-  margin-left: 5px;
+  
 }
 
 .menu{
   font-size: 150%;
   background-color: #E8694D;
-  border-style: solid;  
-  border-radius: 10px;
+  border-style: none;
   border-color: #22364b;
-  margin: 1%;
   padding: 1%;
+  width: 50%;
+  margin-bottom: 20px;
+
+}
+
+#right_button{
+  border-left: solid;
+  border-width: 1px;
+}
+
+#left_button{
+  border-right: solid;
+  border-width: 1px;
 }
 
 .menu:hover{
